ee122f789229ef608f1b71a4e4aff5e1f7bbfdaf
looooopy <giorgio.salmi@stena.com>
2021-09-07 02:13:37 +0200

    - Move: treesitter languages install into a bash script file - Refactor: treesitter filenames and links

---
+++ src/nvim/treesitter/install-langs.sh
@@ -0,0 +1,27 @@
+
+echo_error() {
+  RED='\033[0;31m'
+  NC='\033[0m'
+  >&2 echo -e "$RED$1$NC"
+}
+
+main() {
+  local treesitter_languages=($(cat $XDG_CONFIG_HOME/temp/treesitter/active-langs))
+  local errors=()
+  for language in "${treesitter_languages[@]}"; do
+      nvim --headless +"TSInstallSync ${language}" +q
+      if [ $? -ne 0 ]; then
+          errors+=(" - Lang: '$language' Error: '$?'")
+      fi
+  done
+
+  if [ ${#errors[@]} -ne 0 ]; then
+    echo_error
+    for e in "${errors[@]}"; do
+      echo_error "$e"
+    done
+    exit 1
+  fi
+}
+
+main
