364e5ea6fa2aa0169f699bfa6c9fd486e97df5b3
Looooopy <giorgio.salmi@stenaline.com>
2022-02-07 00:58:03 +0100

    Fix: temporary change docker_compose function

--- scripts/common.sh
+++ scripts/common.sh
@@ -23,11 +23,13 @@ docker_compose() {
   local volume_type
   volume_type="${1:?}"
   shift
-  if [[ "$(uname)" == 'Darwin' ]]; then
-    DEV_UID=1000 DEV_GID=1000 docker-compose  -f "$SRC_ROOT"/docker-compose.yml -f "$SRC_ROOT"/docker-compose-volume-${volume_type}.yml --env-file "$SRC_ROOT"/.env  $@
-  else
+  #if [[ "$(uname)" == 'Darwin' ]]; then
+    DEV_UID=1000 DEV_GID=1000 docker-compose -f "$SRC_ROOT"/docker-compose.yml -f "$SRC_ROOT"/docker-compose-volume-${volume_type}.yml --env-file "$SRC_ROOT"/.env  $@
+  #else
   # Docker dont threat UID and GID as envirnment variables so we have to do it externaly
-  DEV_UID=$(id -u) DEV_GID=$(id -g) docker-compose  -f "$SRC_ROOT"/docker-compose.yml -f "$SRC_ROOT"/docker-compose-volume-${volume_type}.yml --env-file "$SRC_ROOT"/.env  $@
+  #DEV_UID=$(id -u) DEV_GID=$(id -g) docker-compose  -f "$SRC_ROOT"/docker-compose.yml -f "$SRC_ROOT"/docker-compose-volume-${volume_type}.yml --env-file "$SRC_ROOT"/.env  $@
+  #fi
+}
 
 export_host_os() {
   export HOST_OS
