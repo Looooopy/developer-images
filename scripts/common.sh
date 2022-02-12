1889dd5df38939e328efc9e1c425a6b8c3fbabee
looooopy <giorgio.salmi@stena.com>
2022-02-12 15:39:22 +0100

    Refactor: prune script

--- scripts/common.sh
+++ scripts/common.sh
@@ -94,20 +94,3 @@ export_dot_env() {
 
   return 0
 }
-
-
-prune_volumes() {
-  local all_volumes
-  all_volumes=${1:-${ALL_VOLUMES:-no}}
-  if [ "$all_volumes" != "no"  ]; then
-    unset ALL_VOLUMES
-    export GENERIC_VOLUME_TYPE
-    GENERIC_VOLUME_TYPE=container
-    prune_volumes
-    GENERIC_VOLUME_TYPE=localhost
-    prune_volumes
-    return 0
-  elif [[ "${GENERIC_VOLUME_TYPE:?}" == container ]] ||  [[ "${GENERIC_VOLUME_TYPE:?}" == localhost ]]; then
-    docker_compose "${GENERIC_VOLUME_TYPE:?}" down --volumes
-  fi
-}
