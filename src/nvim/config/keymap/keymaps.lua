6e5838b44321d2af384b48c5d055ced624b80d80
looooopy <giorgio.salmi@stena.com>
2022-02-26 04:51:55 +0100

    Nvim; Fix: register keymaps.lua with Whichkey

--- src/nvim/config/keymap/keymaps.lua
+++ src/nvim/config/keymap/keymaps.lua
@@ -1,10 +1,25 @@
 local shortcuts = require('shortcuts')
 local m = require('constants.vim-mode')
+local wk = require('which-key')
 
 -- Leader commands
-shortcuts.map(m.normal, '<leader>r', '<CMD>NvimTreeToggle<CR>')
-shortcuts.map(m.normal, '<leader>n', '<CMD>NvimTreeFindFile<CR>')
-shortcuts.map(m.normal, '<leader>m', '<CMD>MinimapToggle<CR>')
-
--- Ctrl commands
-shortcuts.map(m.normal,'<C-e>', '<CMD>NvimTreeToggle<CR>')
+wk.register(
+  {
+    t = {
+      name = 'Gui toggle',
+      h = {'<cmd>NvimTreeToggle<cr>', 'Hide all'},
+      s = {'<cmd>NvimTreeClose<cr>', 'Show all'},
+    },
+    d = {
+      name = 'Debug',
+    },
+    f = {
+      name = 'Files',
+    },
+  },
+  {
+      prefix = '<leader>',
+      mode = m.normal,
+      buffer = nil,
+  }
+);
