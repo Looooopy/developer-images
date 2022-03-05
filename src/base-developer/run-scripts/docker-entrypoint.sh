85e4d642eace873b6b4451355306f96be8d6eeb7
Looooopy <giorgio.salmi@stenaline.com>
2022-03-05 18:36:11 +0100

    Base-Developer;   Fix: Access docker host as none root user in the same way as ssh agent forwarding.   Fix: Do not create sockets if running as root.   Fix: echo warning when running as root

--- src/base-developer/run-scripts/docker-entrypoint.sh
+++ src/base-developer/run-scripts/docker-entrypoint.sh
@@ -5,15 +5,31 @@ set -e
 source /docker-entrypoint-helper.sh
 
 run_setup_as_root() {
-  if [[ ${HOST_OS} == 'macos' ]]; then
+  if [[ ${HOST_OS} == 'macos' ]] && [[ -z ${RUN_AS_ROOT} ]]; then
     # Note:
     #   Setup a socket through socat to communicate through ssh agent forwarding
     #   via the magic /run/host-services/ssh-auth.sock that is used in MacOS
+    echo "Setting up relay for Agent Forwarding"
     socat UNIX-LISTEN:/home/"${DEV_USER}"/.ssh/socket,fork,user="${DEV_UID}",group="${DEV_GID}",mode=777 \
       UNIX-CONNECT:/run/host-services/ssh-auth.sock \
       &
 
     export SSH_AUTH_SOCK=/home/${DEV_USER}/.ssh/socket
+
+    # Note:
+    #   Setup a socket through socat to communicate with docker
+    echo "Setting up relay for Docker"
+    socat UNIX-LISTEN:/home/"${DEV_USER}"/.docker/docker_socket,fork,user="${DEV_UID}",group="${DEV_GID}",mode=777 \
+      UNIX-CONNECT:/var/run/docker.sock \
+      &
+
+    export DOCKER_HOST=unix:///home/${DEV_USER}/.docker/docker_socket
+  fi
+
+  if [[ -n ${RUN_AS_ROOT} ]]; then
+    echo
+    echo 'Warning: Running all aps as ROOT'
+    echo
   fi
 }
 
